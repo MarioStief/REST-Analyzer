eae8a6439718cd8162769d9b9b8b959317e04b94
Mario Stief <mario@mario-pc.fritz.box>
2012-08-21 01:43:06 +0200

    Layout

--- RESTful Web Analyzer/ViewController.h
+++ RESTful Web Analyzer/ViewController.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate> {
+    NSArray *requestMethods;
+}
+@property (weak, nonatomic) IBOutlet UITextField *url;
+@property (weak, nonatomic) IBOutlet UIPickerView *requestMethod;
+- (IBAction)go:(id)sender;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollview;
+@property (weak, nonatomic) IBOutlet UITableView *httpHeaders;
 
 @end
--- RESTful Web Analyzer/ViewController.m
+++ RESTful Web Analyzer/ViewController.m
@@ -13,15 +13,24 @@
 @end
 
 @implementation ViewController
+@synthesize scrollview;
+@synthesize httpHeaders;
+@synthesize url;
+@synthesize requestMethod;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    requestMethods = [[NSArray alloc] initWithObjects:@"GET", @"POST", @"PUT", @"DELETE", @"HEAD", nil];
 }
 
 - (void)viewDidUnload
 {
+    [self setUrl:nil];
+    [self setRequestMethod:nil];
+    [self setScrollview:nil];
+    [self setHttpHeaders:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +40,36 @@
     return YES;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.url) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    //One column
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    //set number of rows
+    return requestMethods.count;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    //set item per row
+    return [requestMethods objectAtIndex:row];
+}
+
+
+- (IBAction)go:(id)sender {
+    /* if (self.url.beginswith("www.") then "http://" + self.url.text;
+    NSString *self.url.text = ([[NSString alloc] initWithFormat:@"http://%@", self.url.text]);
+     */
+    [self.url resignFirstResponder];
+}
 @end
